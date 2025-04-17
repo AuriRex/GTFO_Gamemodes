f5884b58b4be4d192ddce5905230a95d8c1bb2a6
AuriRex <xaurirexx@gmail.com>
2025-04-17 16:33:23 +0200

    Bump Version v0.5.1

--- GTFO_Gamemodes/Plugin.cs
+++ GTFO_Gamemodes/Plugin.cs
@@ -29,7 +29,7 @@ public class Plugin : BasePlugin
     
     public const string GUID = "dev.aurirex.gtfo.gamemodes";
     public const string NAME = "Gamemodes";
-    public const string VERSION = "0.5.0";
+    public const string VERSION = "0.5.1";
 
     internal static ManualLogSource L;
 

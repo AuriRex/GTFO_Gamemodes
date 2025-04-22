c944b84d0731d8f90b6827a5df46b96d0348c36d
AuriRex <xaurirexx@gmail.com>
2025-04-22 23:40:28 +0200

    Bump Version v0.6.1

--- HNS/Plugin.cs
+++ HNS/Plugin.cs
@@ -17,7 +17,7 @@ public class Plugin : BasePlugin
 {
     public const string GUID = "dev.aurirex.gtfo.gamemode.hns";
     public const string NAME = "Hide and Seek";
-    public const string VERSION = "0.6.0";
+    public const string VERSION = "0.6.1";
 
     internal static ManualLogSource L;
 

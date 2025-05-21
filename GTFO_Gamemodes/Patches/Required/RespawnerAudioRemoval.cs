d03dd6feb9542185c5e93be1540785dabf7fd0c3
AuriRex <xaurirexx@gmail.com>
2025-05-21 22:49:40 +0200

    Prevent sound players on 'Infestation_BaseBlob' from starting

--- GTFO_Gamemodes/Patches/Required/RespawnerAudioRemoval.cs
+++ GTFO_Gamemodes/Patches/Required/RespawnerAudioRemoval.cs
@@ -13,3 +13,15 @@ internal static class EnemyRespawnerVisual__Start__Patch
         return false;
     }
 }
+
+// Tiny blobs in R8B1 dim 1 for example, nightmare blobs or whatever
+[HarmonyPatch(typeof(Infestation_BaseBlob), nameof(Infestation_BaseBlob.Start))]
+internal static class Infestation_BaseBlob__Start__Patch
+{
+    public static readonly string PatchGroup = PatchGroups.REQUIRED;
+    
+    public static bool Prefix()
+    {
+        return false;
+    }
+}
